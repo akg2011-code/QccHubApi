e5a57540b57c4b8ceca0e85546e29ca223e905cc
EngineerAhmad <ahmed.yousseef.7@gmail.com>
2020-12-03 12:14:56 +0200

    AddStaticHTMLFileIndex.html

--- QccHub/Startup.cs
+++ QccHub/Startup.cs
@@ -62,6 +62,11 @@ namespace QccHub
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseDefaultFiles(new DefaultFilesOptions
+            {
+                DefaultFileNames = new
+                 List<string> { "index.html" }
+            });
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
