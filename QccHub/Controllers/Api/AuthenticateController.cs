4bf7d62316a0e0e040f5c9f9b4ec2b4d8b58ced1
EngineerAhmad <ahmed.yousseef.7@gmail.com>
2020-12-03 13:08:39 +0200

    AddRegistrationProcess

---
+++ QccHub/Controllers/Api/AuthenticateController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QccHub.Data;
+using QccHub.Data.DTOS;
+
+namespace QccHub.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private ApplicationDbContext Context;
+        UserManager<User> UserManager;
+        RoleManager<IdentityRole> RoleManager;
+        SignInManager<User> SignInManager;
+        public AuthenticateController(ApplicationDbContext _Context
+            , UserManager<User> _userManager
+            , RoleManager<IdentityRole> _roleManager,
+            SignInManager<User> _SignInManager
+            )
+        {
+            Context = _Context;
+            UserManager = _userManager;
+            RoleManager = _roleManager;
+            SignInManager = _SignInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser([FromBody]UserRegisteration user)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await Context.Users.FirstOrDefaultAsync(u => u.PhoneNumber == user.PhoneNumber || u.Email == user.Email);
+                if (result != null)
+                    return BadRequest("there is a user with this information");
+                else
+                {
+                    User newUser = new User
+                    {
+                        UserName = user.UserName,
+                        NormalizedUserName = user.UserName.ToUpper(),
+                        Email = user.Email,
+                        NormalizedEmail = user.Email.ToUpper(),
+                        EmailConfirmed = true,
+                        PhoneNumber = user.PhoneNumber,
+                        PhoneNumberConfirmed = true,
+                        TwoFactorEnabled = true,
+                        LockoutEnabled = false,
+                        AccessFailedCount = 1,
+                    };
+
+                    var result2 = await UserManager.CreateAsync(newUser, user.Password);
+                    if (result2.Succeeded)
+                    {
+                        await UserManager.AddToRoleAsync(newUser, "User");
+                        return Ok("user registered successfully");
+                    }
+                    else
+                    {
+                        return BadRequest(result2.Errors.ToList()[0].Description);
+                    }
+                }
+            }
+            return BadRequest("Invalid Form");
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterCompany([FromBody]UserRegisteration user)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await Context.Users.FirstOrDefaultAsync(u => u.PhoneNumber == user.PhoneNumber || u.Email == user.Email);
+                if (result != null)
+                    return BadRequest("there is a user with this information");
+                else
+                {
+                    User newUser = new User
+                    {
+                        UserName = user.UserName,
+                        NormalizedUserName = user.UserName.ToUpper(),
+                        Email = user.Email,
+                        NormalizedEmail = user.Email.ToUpper(),
+                        EmailConfirmed = true,
+                        PhoneNumber = user.PhoneNumber,
+                        PhoneNumberConfirmed = true,
+                        TwoFactorEnabled = true,
+                        LockoutEnabled = false,
+                        AccessFailedCount = 1,
+                        IsTrusted = false
+                    };
+
+                    var result2 = await UserManager.CreateAsync(newUser, user.Password);
+                    if (result2.Succeeded)
+                    {
+                        await UserManager.AddToRoleAsync(newUser, "Company");
+                        return Ok("user registered successfully");
+                    }
+                    else
+                    {
+                        return BadRequest(result2.Errors.ToList()[0].Description);
+                    }
+                }
+            }
+            return BadRequest("Invalid Form");
+
+        }
+
+    }
+}
---
+++ QccHub/Controllers/Api/JobsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QccHub.Data;
+
+namespace QccHub.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        ApplicationDbContext Context;
+        public JobsController(ApplicationDbContext _context)
+        {
+            Context = _context;
+        }
+
+
+    }
+}
