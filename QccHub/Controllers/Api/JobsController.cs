ce3b411b53e9354aff99d0bee2995b954ae361be
EngineerAhmad <ahmed.yousseef.7@gmail.com>
2020-12-03 17:04:18 +0200

    AddSearchJobsApi

--- QccHub/Controllers/Api/JobsController.cs
+++ QccHub/Controllers/Api/JobsController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using QccHub.Data;
+using QccHub.DTOS;
 
 namespace QccHub.Controllers.Api
 {
@@ -14,9 +17,11 @@ namespace QccHub.Controllers.Api
     public class JobsController : ControllerBase
     {
         ApplicationDbContext Context;
-        public JobsController(ApplicationDbContext _context)
+        private readonly IWebHostEnvironment webHostEnvironment;
+        public JobsController(ApplicationDbContext _context, IWebHostEnvironment _webHostEnvironment)
         {
             Context = _context;
+            webHostEnvironment = _webHostEnvironment;
         }
 
         [HttpPost]
@@ -57,7 +62,7 @@ namespace QccHub.Controllers.Api
                 return NotFound("No company for this ID");
             else
             {
-                IEnumerable<Job> companyJobs = Context.Job.Where(j=>j.CompanyID == companyID).Include(j=>j.Company);
+                IEnumerable<Job> companyJobs = Context.Job.Where(j => j.CompanyID == companyID).Include(j => j.Company);
                 return Ok(companyJobs);
             }
         }
@@ -75,7 +80,71 @@ namespace QccHub.Controllers.Api
         [HttpGet]
         public IActionResult GetAllJobs()
         {
-            return Ok(Context.Job.OrderByDescending(j=>j.CreatedDate).Include(j=>j.Company));
+            return Ok(Context.Job.OrderByDescending(j => j.CreatedDate).Include(j => j.Company));
+        }
+
+        [HttpGet("{JobName}")]
+        public IActionResult SearchJobs(string JobName)
+        {
+            IEnumerable<Job> jobs = Context.Job.Where(j => j.Title.Contains(JobName) && j.IsDeleted == false).Include(j=>j.Company);
+            return Ok(jobs);
+        }
+        // --------------------------- apply to job ------------------------------
+        [HttpPost]
+        public IActionResult ApplyToJob([FromForm]JobApplications JobApplication)
+        {
+            if (JobApplication.cvFile != null && JobApplication.cvFile.Length > 0)
+            {
+                string cvFileName = Guid.NewGuid().ToString() + JobApplication.cvFile.FileName;
+                string cvFilePath = Path.Combine(webHostEnvironment.WebRootPath + "\\JobsAppliedCV", cvFileName);
+                using (var stream = new FileStream(cvFilePath, FileMode.Create))
+                {
+                    JobApplication.cvFile.CopyTo(stream);
+                }
+                ApplyJobs applyJobs = new ApplyJobs
+                {
+                    IsApproved = false,
+                    ExpectedSalary = JobApplication.ExpectedSalary,
+                    CurrentSalary = JobApplication.CurrentSalary,
+                    CreatedBy = JobApplication.UserID,
+                    CreatedDate = DateTime.Now,
+                    UserID = JobApplication.UserID,
+                    JobID = JobApplication.JobID,
+                    Message = JobApplication.Message,
+                    IsDeleted = false,
+                    CVFilePath = cvFileName
+                };
+                
+                Context.ApplyJobs.Add(applyJobs);
+                Context.SaveChanges();
+                return Ok($"user applyed to job");
+            }
+            else
+                return BadRequest();
+        }
+
+        [HttpGet("{jobID}")]
+        public IActionResult GetAllJobApplications(int jobID)
+        {
+            Job job = Context.Job.Find(jobID);
+            if (job == null)
+                return NotFound("No job found for this ID");
+            IEnumerable<ApplyJobs> jobApplications = Context.ApplyJobs.Where(j=>j.JobID == jobID).Include(j=>j.User).Include(j=>j.Job);
+            return Ok(jobApplications);
+        }
+
+        [HttpPost()]
+        public IActionResult ApproveJobApplication(JobApprove jobApprove)
+        {
+            ApplyJobs applyJobs = Context.ApplyJobs.FirstOrDefault(j=>j.JobID == jobApprove.JobID && j.UserID == jobApprove.UserID);
+            if (applyJobs != null)
+            {
+                applyJobs.IsApproved = true;
+                Context.SaveChanges();
+                return Ok($"user : {Context.Users.Find(applyJobs.UserID).UserName} is approved to job : {Context.Job.Find(applyJobs.JobID).Title}");
+            }
+            else
+                return NotFound();
         }
 
     }
---
+++ QccHub/DTOS/JobApplications.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QccHub.DTOS
+{
+    public class JobApplications
+    {
+        public string UserID { get; set; }
+        public int JobID { get; set; }
+        public string Message { get; set; }
+        public int CurrentSalary { get; set; }
+        public int ExpectedSalary { get; set; }
+        public IFormFile cvFile { get; set; }
+    }
+}
